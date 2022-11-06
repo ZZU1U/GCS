27f45217570d53445223dcc5317dd4c6fdf4c558
gleb <gl.anohin@yandex.ru>
2022-11-06 16:53:03 +0700

    as we can

--- asd.py
+++ asd.py
@@ -2,46 +2,65 @@ import cv2
 import mediapipe as mp
 import numpy as np
 
+#define gesture type
+def gesture_type(s):
+    if s.item(5,0)-s.item(17,0) < -30:
+        print("reverse",end=" ")
+    elif s.item(5,0)-s.item(17,0) > 30:
+        print("just",end=" ")
+    else:
+        print("side",end=" ")
+    if abs(s.item(5, 1) - s.item(1,1)) > 80:
+        print("не в камеру")
+    else:
+        print("в камеру")
+
+
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
 
-# For webcam input:
-cap = cv2.VideoCapture(0)
+# Main cycle
+cap = cv2.VideoCapture(1)
 with mp_hands.Hands(
     model_complexity=0,
     min_detection_confidence=0.5,
     min_tracking_confidence=0.5) as hands:
   while cap.isOpened():
     success, image = cap.read()
+
+    # Error detecting
+
     if not success:
       print("Ignoring empty camera frame.")
-      # If loading a video, use 'break' instead of 'continue'.
       continue
 
-    # To improve performance, optionally mark the image as not writeable to
-    # pass by reference.
+    # image capture
+
     image.flags.writeable = False
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     results = hands.process(image)
     ids = np.matrix([[0, 0]])
-    # Draw the hand annotations on the image.
+
+    # Draw the hand annotations on the image
+
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     if results.multi_hand_landmarks:
+
+        # multi you can change to right or left for each hand
+
       for handLms in results.multi_hand_landmarks:
           for id, lm in enumerate(handLms.landmark):
+              # write to ids
               h, w, c = image.shape
-              cx, cy = int(lm.x * w), int(lm.y * h)
+              cx, cy, cz = int(lm.x * w), int(lm.y * h), float(lm.z)
               if id == 0:
-                  nule = [cx, cy]
-              if id == len(ids) - 1:
-                  ids = np.append(ids, [[cx - nule[0], cy - nule[1]]], axis=0)
+                  ids = np.array([[0, 0],[cx, cy]])
+              else:
+                  ids = np.append(ids, [[cx-ids.item(1,0), cy-ids.item(1,1)]], axis=0)
           ids = np.delete(ids, 0, 0)
-          minl = ((ids.item(5,0)-ids.item(0,0))**2+(ids.item(5,1)-ids.item(0,1))**2)**0.5
-          for i in range(len(ids)):
-              cv2.circle(image, (int(ids.item(i,0)*100/minl)+200,int(ids.item(i,1)*100/minl)+400), 3,(255,255,255), 3)
-          print(ids/minl)
+          gesture_type(ids)
     # Flip the image horizontally for a selfie-view display.
     cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
     if cv2.waitKey(5) & 0xFF == 27:
