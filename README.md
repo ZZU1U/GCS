45b526802033f65fcbc3dc19adf589fb2df91e8f
zulu <86395684+ZZU1U@users.noreply.github.com>
2022-11-20 17:57:38 +0700

    new
    new

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ZUZ
  СУЖ - системи управления жестами
+ notion: https://rogue-nigella-cbb.notion.site/d10bd81fba6346fbbe745c15b819d1c5
--- asd.py
+++ asd.py
@@ -2,21 +2,27 @@ import cv2
 import mediapipe as mp
 import numpy as np
 
-#define gesture type
+lastids = np.array()
+
+
+print("Press esc to escape")
+
+# define gesture type
+
 def gesture_type(s):
     # detect by x
 
-    if s.item(5,0)-s.item(17,0) < -30:
-        print("reverse",end=" ")
+    if s.item(5, 0)-s.item(17, 0) < -30:
+        print("reverse", end=" ")
 
-    elif s.item(5,0)-s.item(17,0) > 30:
-        print("just",end=" ")
+    elif s.item(5, 0)-s.item(17, 0) > 30:
+        print("just", end=" ")
     else:
-        print("side",end=" ")
+        print("side", end=" ")
 
     # detect by y
 
-    if abs(s.item(5, 1) - s.item(1,1)) > 80:
+    if abs(s.item(5, 1) - s.item(1, 1)) > 80:
         print("не в камеру")
     else:
         print("в камеру")
@@ -31,44 +37,50 @@ cap = cv2.VideoCapture(1)
 with mp_hands.Hands(
     model_complexity=0,
     min_detection_confidence=0.5,
-    min_tracking_confidence=0.5) as hands:
-  while cap.isOpened():
-    success, image = cap.read()
+    min_tracking_confidence=0.5
+) as hands:
+    while cap.isOpened():
+        success, image = cap.read()
 
-    # Error detecting
+        # Error detecting
 
-    if not success:
-      print("Ignoring empty camera frame.")
-      continue
+        if not success:
+            print("Ignoring empty camera frame.")
+            continue
 
-    # image capture
+        # image capture
 
-    image.flags.writeable = False
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    results = hands.process(image)
-    ids = np.matrix([[0, 0]])
+        image.flags.writeable = False
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        results = hands.process(image)
+        ids = np.matrix([[0, 0]])
 
-    # Draw the hand annotations on the image
+        # Draw the hand annotations on the image
 
-    image.flags.writeable = True
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    if results.multi_hand_landmarks:
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        if results.multi_hand_landmarks:
 
-        # multi you can change to right or left for each hand
+            # multi you can change to right or left for each hand
 
-      for handLms in results.multi_hand_landmarks:
-          for id, lm in enumerate(handLms.landmark):
-              # write to ids
-              h, w, c = image.shape
-              cx, cy, cz = int(lm.x * w), int(lm.y * h), int(lm.z*c)
-              print(cz)
-              if id == 0:
-                  ids = np.array([[0, 0],[cx, cy]])
-              else:
-                  ids = np.append(ids, [[cx-ids.item(1,0), cy-ids.item(1,1)]], axis=0)
-          ids = np.delete(ids, 0, 0)
-          gesture_type(ids)
-    cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
-    if cv2.waitKey(5) & 0xFF == 27:
-      break
+            for handLms in results.multi_hand_landmarks:
+                for id, lm in enumerate(handLms.landmark):
+                    # write to ids
+                    h, w, c = image.shape
+                    cx, cy = int(lm.x * w), int(lm.y * h)
+                    if id == 0:
+                        ids = np.array([[0, 0], [cx, cy]])
+                    else:
+                        ids = np.append(ids, [[cx-ids.item(1, 0), cy-ids.item(1,  1)]], axis=0)
+                ids = np.delete(ids, 0, 0)
+                if lastids.size < 3:
+                    lastids = np.append(lastids, [ids], axis=2)
+                else:
+                    lastids = np.append(lastids, [ids], axis=2)
+                    lastids = np.delete(lastids, 0)
+                print(lastids)
+        cv2.imshow('MediaPipe', cv2.flip(image, 1))
+        if cv2.waitKey(5) & 0xFF == 27:
+            break
+
 cap.release()
